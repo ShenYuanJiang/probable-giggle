1bb34581f54fc7db1b98bc96bb74cc032c287d7e
shenyuanjiang <1223551803@qq.com>
2018-02-06 14:20:14 +0800

    first cmmit

---
+++ QQ/QQ/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QQ
+//
+//  Created by yuanjiang on 2018/2/6.
+//  Copyright © 2018年 yuanjiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
