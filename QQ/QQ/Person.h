d1541e2cdca71cb91503f9c864baca9fa622f2f8
shenyuanjiang <1223551803@qq.com>
2018-02-06 14:35:57 +0800

    fffff

---
+++ QQ/QQ/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  QQ
+//
+//  Created by yuanjiang on 2018/2/6.
+//  Copyright © 2018年 yuanjiang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ QQ/QQ/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  QQ
+//
+//  Created by yuanjiang on 2018/2/6.
+//  Copyright © 2018年 yuanjiang. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
